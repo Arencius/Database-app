38aaf0d1e7121fb0f8637d6b608c079ddb6a5d53
Karol Krawczyk <42814139+Arencius@users.noreply.github.com>
2020-06-14 13:23:24 +0200

    Update Song.java

--- app/src/main/java/com/example/database_app/Song.java
+++ app/src/main/java/com/example/database_app/Song.java
@@ -14,17 +14,9 @@ public class Song {
         this.views = views;
     }
 
-    public String getTitle() {
-        return title;
-    }
-    public String getArtist() {
-        return artist;
-    }
-    public String getAlbum() {
-        return album;
-    }
-    public String getDuration() {
-        return duration;
-    }
+    public String getTitle() { return title; }
+    public String getArtist() { return artist; }
+    public String getAlbum() { return album; }
+    public String getDuration() { return duration; }
     public int getViews() { return views; }
 }
